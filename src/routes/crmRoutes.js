6b2f26e7232ad101408af58a0f57406844a2a6cd
Wadly dugue <dukyw1@gmail.com>
2022-06-19 22:15:40 -0300

    Delete endpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,24 +1,30 @@
-import { addNewContact, getContacts, getContactWithID, updateContact } from "../controllers/crmController";
+import {
+  addNewContact,
+  deleteContact,
+  getContacts,
+  getContactWithID,
+  updateContact,
+} from "../controllers/crmController";
 
 const routes = (app) => {
-    app.route('/contact')
-        .get((req, res, next) => {
-            //middleware
-            console.log(`Request from: ${req.originalUrl}`)
-            console.log(`Request from: ${req.method}`)
-            next();
-        }, getContacts)
+  app
+    .route("/contact")
+    .get((req, res, next) => {
+      //middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request from: ${req.method}`);
+      next();
+    }, getContacts)
 
-        .post(addNewContact);
+    .post(addNewContact);
 
-    app.route('/contact/:contactID')
-        .get(getContactWithID)
+  app
+    .route("/contact/:contactID")
+    .get(getContactWithID)
 
-        .put(updateContact)
-        
-        .delete((req, res) =>
-            res.send('DELETE request successfull!')  
-        );
-}
+    .put(updateContact)
+
+    .delete(deleteContact);
+};
 
 export default routes;
