365e6afeb9e6534c2e732c9dfdb2310533889414
Wadly dugue <dukyw1@gmail.com>
2022-06-19 21:50:40 -0300

    GET  specific ID enpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,4 +1,4 @@
-import { addNewContact, getContacts } from "../controllers/crmController";
+import { addNewContact, getContacts, getContactWithID } from "../controllers/crmController";
 
 const routes = (app) => {
     app.route('/contact')
@@ -12,6 +12,7 @@ const routes = (app) => {
         .post(addNewContact);
 
     app.route('/contact/:contactID')
+        .get(getContactWithID)
         .put((req, res) =>
             res.send('PUT request successfull!')  
         )
